72b52a1dac3adb35456159ff0f967687b812d725
Sophie Lee <sophie.jiseon.lee@gmail.com>
2015-09-18 18:06:46 -0400

    creating folders

--- intro.R
+++ intro.R
@@ -1,4 +1,10 @@
-#####
-#####  git@github.com:sophielee1/MachineLearningProject.git
-#####
-setwd('/Users/sophie/Documents/Data')
+#####################################
+### Probabilistic Machine Learning 
+### Fall, 2015
+### Howard, Haohan, Sophie 
+### Git up repository -- git@github.com:sophielee1/MachineLearningProject.git
+#####################################
+
+# setwd('/Users/sophie/Documents/Data')
+# 
+
