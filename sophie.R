04733f7b236c7a04fa5166a7241f82b6d367e2e0
Sophie Lee <sophie.lee@duke.edu>
2015-11-26 00:21:29 -0500

    Fixed the listing method and some bugs in the pre-treatment stage I.

--- sophie.R
+++ sophie.R
@@ -9,13 +9,16 @@ library("openNLP")
 library(plyr) # ddply function in the parse_sentences function
 library(tidyr) #spread function in the parse_sentences function
 library(stringr) #string matching
-library(tm)
+library(tm) #stem words, etc.
+library(magrittr)
+
 
 ##### Load data ####
 
 icews <- read.csv(paste0(base.path, "/all_protest_from2001_2014_20151111.csv"))
 icews$headline = as.character(icews$headline)
 icews$text = as.character(icews$text)
+
 
 
 ##### helper function ####
@@ -64,17 +67,22 @@ parse_sentences <- function(text, aggregation="sentence") {
                                    .variables=c("sentence_number","features_category") ,  mutate,
                                    features_collapsed = paste(string, collapse="; " ) )
   
-  subset_sentence_feature <- subset(sentence_annotations_df, !is.na(features_category) & features_category !="NA",  select= c(sentence_number,features_category,features_collapsed) )
+  subset_sentence_feature <- subset(sentence_annotations_df, !is.na(features_category) & features_category !="NA",  
+                                    select= c(sentence_number,features_category,features_collapsed) )
   
   condition <- !duplicated(subset_sentence_feature[,c("sentence_number","features_category")])
+  
   subset_sentence_feature <- subset_sentence_feature[condition,c("sentence_number","features_category","features_collapsed")]
   
   subset_sentence_feature <- spread(data=subset_sentence_feature, key=features_category , value=features_collapsed  )
   
-  subset_sentence_feature$sentence_string <-setence_strings$string
+  sentence_number<-unique(subset_sentence_feature$sentence_number)
+  
+  subset_sentence_feature$sentence_string <-setence_strings$string[sentence_number]
   
   return(subset_sentence_feature)
 }
+
 
 
 # Create pipeline: Combine individual annotators into a single "pipeline".
@@ -93,49 +101,106 @@ pipeline <- list(sent_token_annotator,word_token_annotator, pos_tag_annotator, #
                  date_ann)
 
 
-##### STEP 1: Pre-treatment ####
+##### STEP 1: (Pre-treatment I) Cut out the parts citing sources ####
+
+
+
 
 
 #1. remove the parts that contain the location of news reporting agencies
-sources<-c("AFP","Reuters","AP","in Chinese")#stopwords
+sources<-c("AFP","Reuters","AP","in Chinese", "web site")#stopwords
 #2. remove the last sentence if it contains the word "source:"
-indicators<-c("Source","press")
+indicators<-c("Description of","Source")
 
 data=icews
+
 newvar<-rep(NA, nrow(data))
 # names(newvar)<-"newvar"
 for(i in 1:length(newvar)){
   sentences<- parse_sentences(data$text[i]) 
-  cell<-sentences[1,9]
+  cell<-sentences[1,ncol(sentences)]
   
   ## IF
   TF<-str_detect(cell,sources)
-  if(length(TF[TF==TRUE])>0){
+  if(length(which(TF==TRUE))>0){
     no<-which(TF==TRUE)
-    split <-str_split(cell,sources)
-    newcell<-split[no][[1]][2]
+        if(length(no)>1){   # if no is longer than 1, use the first case
+        no<-no[1]
+        }
+    src<-sources[no]
+    split <-str_split(cell,src)
+    newcell<-split[[1]][2]
+    sentences[1,ncol(sentences)]<-newcell
   }
   ### End of IF 1
   
-  sentences[1,9]<-newcell
+  
   
   ## IF 2
   
-  cell<-sentences[nrow(sentences),9]
+  cell<-sentences[nrow(sentences),ncol(sentences)]
   TF<-str_detect(cell,indicators)
-  if(length(TF[TF==TRUE])>0){
-    sentences=sentences[1:nrow(sentences)-1,]   
+  if(length(which(TF==TRUE))>0){
+ 
+    no<-which(TF==TRUE)
+        if(length(no)>1){  # if no is longer than 1, use the first case
+        no<-no[1]
+        }
+    indicator<-indicators[no]
+    split <-str_split(cell,indicator)
+    newcell<-split[[1]][1]
+        if(length(newcell)==0){  #if the indicator words appears at the beginning
+        newcell<-"\n\n"
+        }
+    sentences[nrow(sentences),ncol(sentences)]<-newcell
   }
   
   ## End of IF 2
+#   sentences=sentences[1:nrow(sentences)-1,]
   
-  coerced<-as.String(sentences[,9])
+  coerced<-as.String(sentences[,ncol(sentences)])
   newvar[i]<-as.character(coerced)
 }
 
 
 
 data=cbind(data,newvar)
+
+
+
+
+##### Step 2: (Pre-Treatment II) stem words & get rid of special characters etc. #####
+
+# icews$text = str_replace_all(icews$text,"\\?\\???","") %>% # rid of question marks
+#    str_replace_all("[","") %>%  # rid of [Desciption of source: ...]
+#    str_replace_all("(","") %>% # rid of (2500 chrts; jjbd1031a)
+#   str_replace_all("]","") %>%  # rid of [Desciption of source: ...]
+#   str_replace_all(")","") %>% # rid of (2500 chrts; jjbd1031a)  
+#    str_replace_all("http\\S+\\s*", "") %>%  # rid of http
+#    str_replace_all("Attachments:", "") %>%  # we need sentences
+#   str_replace_all("Guangzhou$", "") %>%
+#   str_replace_all("?", "") %>%
+#   str_replace_all("\n\n.*pdf$", " ") %>% # get rid of ending with pdf
+#   str_replace_all("\n\nsai.*?$", "") %>% # row 26, 68
+#   str_replace_all("\n\nbur.*?$", "") %>% # row 589 -> \n\nbur/slb/dwa
+#   str_replace_all(".com","") %>%
+#   str_replace_all("Apr.", "April") %>% # row632
+#    str_replace_all("\\$|\\=|\\(|\\)|-|--|'|\"|\\.\\.\\.|_|______|_____|:|\\*", " ")  # rid of symbols (always the last)
+
+### need to run the parse_sentences function below first
+# df <- list()
+# 
+# for (i in 1:642){
+#   text=icews$text[i]
+#   df[[i]] <- parse_sentences(icews$text[i])
+#   # Anhui (human: Zhejiang, because workers from Anhui protested in Zhejing province)
+#   print(i)
+# }
+# 
+# View(df[[11]])
+# icrews.df = lapply(df, as.data.frame())
+# write.csv(icrews.df, file = "/Users/howardliu/dropbox/Chinese NLP project/Data/icewslists.csv")
+
 
 
 
@@ -155,6 +220,9 @@ testdata<-as.data.frame(terms(gibbs, 10))
 
 # k = n, extract LDA matrices  -> run NLP again for actors 
 
+# 
+# treat text -- base formn words
+# cluster
 
 
 
