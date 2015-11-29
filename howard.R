c59790acb048e2ce35822f798aff070de953e027
Howard Liu <haoliuhoward@gmail.com>
2015-11-29 09:08:56 -0500

    Update howard.R
    update on location extracting

--- howard.R
+++ howard.R
@@ -1,5 +1,8 @@
 ### howard's code here #
 
+load("/Users/howardliu/dropbox/Chinese NLP project/Data/icews_nlp.RData")
+loc = read.csv("/Users/howardliu/dropbox/Chinese NLP project/codebook_location.csv")
+province = as.character(loc$Province)
 
 
 
