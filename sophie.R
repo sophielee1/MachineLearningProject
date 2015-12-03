406599c52f175e11071e023dea2b30d42c7194b2
Sophie Lee <sophie.lee@duke.edu>
2015-12-03 17:07:31 -0500

    Sophie's annotated script. This is the final version of the results reported for the machine learning class.

--- sophie.R
+++ sophie.R
@@ -1,17 +1,34 @@
+##################################
+# Sophie Lee
+# Machine Learning Class final project: Annotated script
+# Dec. 3rd. 2015.
+##################################
+
+
 rm(list=ls())
 
+
 ##### Set up ####
-setwd("/Users/sophie/Documents/Data/")
+setwd("/Users/sophie/Documents/Data/")  # my working path
 base.path=getwd()
-library("openNLPmodels.en")
+
+
+library("openNLPmodels.en") # NLP dictionary for English
 library("NLP")
-library("openNLP")
+library("openNLP") 
 library(plyr) # ddply function in the parse_sentences function
 library(tidyr) #spread function in the parse_sentences function
 library(stringr) #string matching
 library(tm) #stem words, etc.
-library(magrittr)
-library(topicmodels)
+library(magrittr) # for speedy processing of data management
+library(topicmodels) # fitting the LDA model
+
+# packages for plotting
+library(rgdal) #reading the shape files
+library(maptools) # reading shape files
+library(RColorBrewer) # for palettes and color schemes
+library(classInt) # for defining intervals
+
 
 
 ##### Load data ####
@@ -21,7 +38,8 @@ icews$headline = as.character(icews$headline)
 icews$text = as.character(icews$text)
 
 
-##### helper function ####
+
+##### helper functions ####
 parse_sentences <- function(text, aggregation="sentence") {
   
   nouns <- c("NN")
@@ -129,6 +147,7 @@ score<-function(youranswer, solution){
 }
 
 
+
 ##### STEP 1: (Pre-treatment I) Cut out the parts citing sources ####
 
 
@@ -225,7 +244,7 @@ write.csv(data, "Chinese_nlp_treated_data_sophie.csv")
 
 
 
-##### STEP 2: search for provinces & actors ####
+##### STEP 3: search for provinces & actors ####
 
 ### set up
 
@@ -378,78 +397,198 @@ score(actor_machine, actor_ICEWS)
 
 data_new=as.data.frame(cbind(data, province_machine, actor_machine, actor_ICEWS))
 write.csv(data_new, "Sophie_1201_data.csv")
-##### STEP 3: Plotting ####
+data=data_new
 
 
 
 
 
+##### STEP 4: Plotting ####
 
 
 
-# 
-# 
-# # delete China
-# #get k
-# 
-# count_names(for i in 1:length(nrow(data))){
-#   test<-parse_sentences(newvar[i])
-#   
-# }
-# 
-# nlp=list()
-# for(i in 1:length(data$newvar))
-#   nlp[[i]]<-
-#   lll<-parse_sentences(newvar[i]) # to data frame
-# 
-# 
-# view(nlp[[i]])
-# string<-GetCorpus(newvar[7])
-# dtm<-DocumentTermMatrix(string)
+par(mfrow=c(1,3))
+par(mar=c(0,0,1,0))
+data=read.csv("Sophie_1201_data.csv")
 
-# testfinal<-parse_sentences(testdata[,1])
-# testdata<-as.data.frame(terms(gibbs, 10))
-
-# run NLP -> then get the number of location identifiers, N
+colors = brewer.pal(9, "Spectral")
 
 
+# names<-unique(names)
+# names<-str_replace(names,"\n"," ")
+# 
+# name_cells<-strsplit(as.character(names, " "))
+# 
+# protestdata <- data_new
+chinaShape<-readShapePoly('/Users/sophie/Documents/Data/CHN_adm1.shp')
 
-# 
-# 
-# 
-# 
-# gibbs=LDA(dtm, k=k, method="Gibbs",
-#           control=list(seed=SEED, burnin=burn, thin=odens, iter=sims))
-# df=rbind(dtm[[3]],dtm[[6]][2])
-# 
-# df<-DocumentTermMatrix(df)
-# freqwords<-findFreqTerms(dtm, 3)
-# 
-# 
-# 
-# freqency=sort(dtm[[3]], decreasing=T)
-# frequentest<-dtm[[6]][[2]][order(dtm[[3]], decreasing=T)]
-# 
-# wordcloud(frequentest, freqency, 
-#           max.words=30, scale=c(3.5, .5), use.r.layout=F)
-# 
-# #The top 5 terms under the 10 topics are here
-# terms(gibbs, 10)
-# 
-# 
-# 
-# actors_dic<-unique(data$source_actor)
-# 
-# 
-# 
-# # k = n, extract LDA matrices  -> run NLP again for actors 
-# 
-# 
-# 
-# # 
-# # treat text -- base formn words
-# # cluster
-# 
-# 
-# 
+#make the province name convention consistent
 
+provsShape<-chinaShape@data %>% .$NAME_1 %>% unique() %>% as.character()
+provsShapeKey = provsShape
+provsShape[provsShape=='Xizang'] = 'Tibet'
+provsShape[provsShape=='Ningxia Hui'] = 'Ningxia'
+provsShape[provsShape=='Nei Mongol'] = 'Inner-Mongolia'
+provsShape[provsShape=='Xinjiang Uygur'] = 'Xinjiang'
+provsData = data.frame(provsShape, provsShapeKey, stringsAsFactors=FALSE)
+provsData$id = 1:nrow(provsData)
+provsData[,1]=tolower(provsData[,1])
+
+
+
+# (1) Human coded
+names<-data$province_human
+# names<-str_replace(names,"hong kong","hong-kong")
+# names<-str_trim(names)
+names<-tolower(names)
+names<-str_replace(names, "inner mongolia","inner-mongolia")
+unique(names)
+
+
+# count variable
+names_list=names
+tab<-table(names_list)
+
+count=as.vector(rep(NA, nrow(provsData)))
+for(i in 1:nrow(provsData)){
+  TF<- names(tab)==provsData[i,1] 
+  
+  if( length(which(TF==TRUE)) > 0 ) {
+    count[i]<-as.numeric(tab[which(names(tab)==provsData[i,1])])
+  } else {
+    count[i]=0
+  }
+}
+
+provsData=as.data.frame(cbind(provsData, "count"=count))
+
+
+chinaShape$count = provsData$count[match(chinaShape@data$NAME_1, provsData$provsShapeKey)]
+
+brks = classIntervals(chinaShape$count , n=9, style="fixed", 
+                      fixedBreaks=c( 0, 15, 30, 45, 60, 75, 90, 105 ,120, 151) )  ### choose "equal or not"
+
+brks = brks$brks
+plot(chinaShape,
+     col=colors[findInterval(chinaShape$count , brks,all.inside=TRUE)],
+     axes=F,
+     main="Human-coded", cex.main=1.2)
+
+
+
+
+
+# (2) ICEWS
+names=as.vector(as.character(data$province_ICEWS)) #provinces
+remove<-c("Sheng","Shi","Autonomous Region","Zizhiqu")
+for(i in 1:length(remove)){
+  names<-str_replace_all(names,remove[i],"") 
+}
+
+names<-str_trim(names)
+names<-tolower(names)
+unique(names)
+
+names<-str_replace(names,"xinjiang uygur","xinjiang")
+names<-str_replace(names, "inner mongolia","inner-mongolia")
+names<-str_replace(names,"guangxi zhuangzu","guangxi")
+
+# count variable
+names_list=names
+tab<-table(names_list)
+
+count=as.vector(rep(NA, nrow(provsData)))
+for(i in 1:nrow(provsData)){
+  TF<- names(tab)==provsData[i,1] 
+  
+  if( length(which(TF==TRUE)) > 0 ) {
+    count[i]<-as.numeric(tab[which(names(tab)==provsData[i,1])])
+  } else {
+    count[i]=0
+  }
+}
+
+provsData=as.data.frame(cbind(provsData, "count"=count))
+
+
+chinaShape$count = provsData$count[match(chinaShape@data$NAME_1, provsData$provsShapeKey)]
+
+brks = classIntervals(chinaShape$count , n=9, style="fixed", 
+                      fixedBreaks=c( 0, 10, 20, 30, 40, 50, 60, 70 , 80, 151) )  ### choose "equal or not"
+
+brks = brks$brks
+plot(chinaShape,
+     col=colors[findInterval(chinaShape$count , brks,all.inside=TRUE)],
+     axes=F,
+     main="ICEWS", cex.main=1.2)
+
+
+
+
+
+# (3) Machine coded data
+
+# make the count variable 
+
+names<-data$province_machine
+names<-str_replace(names,"hong kong","hong-kong")
+names<-str_replace(names, "inner mongolia","inner-mongolia")
+
+
+names_list<-unlist(strsplit(names[1], " "))
+for(i in 2:nrow(data)){
+  if(names[i]==names[i-1]){
+    names_list=c(names_list, NA)
+  } else{
+    names_list<-c(names_list, 
+                  unlist(strsplit(names[i], " ")) 
+    )
+  }
+}
+names_list=names_list[which(!is.na(names_list))]
+
+
+
+tab<-table(names_list)
+
+count=as.vector(rep(NA, nrow(provsData)))
+for(i in 1:nrow(provsData)){
+  TF<- names(tab)==provsData[i,1] 
+
+  if( length(which(TF==TRUE)) > 0 ) {
+  count[i]<-as.numeric(tab[which(names(tab)==provsData[i,1])])
+  } else {
+    count[i]=0
+  }
+}
+
+provsData=as.data.frame(cbind(provsData, "count"=count))
+
+
+chinaShape$count = provsData$count[match(chinaShape@data$NAME_1, provsData$provsShapeKey)]
+
+brks = classIntervals(chinaShape$count , n=9, style="fixed", 
+                      fixedBreaks=c( 0, 15, 30, 45, 60, 75, 90, 105 ,120, 151) )  ### choose "equal or not"
+
+brks = brks$brks
+plot(chinaShape,
+     col=colors[findInterval(chinaShape$count , brks,all.inside=TRUE)],
+     axes=F,
+     main="Machine-coded", cex.main=1.2)
+
+
+
+#add a legend
+
+legend(
+  x=122, y=28,
+  #   'bottomright', # or lazy positioning
+  legend=leglabs(brks),
+  fill=colors,
+  bty="n",x.intersp = .5, y.intersp = .5, cex=0.9)
+
+
+
+
+
+########## End of the codes
